6aa999c627e32af8bd43e1554c03949d6073ec01
Santiago Bandiera <santiagobandiera@MacBook-Pro-de-Santiago.local>
2017-02-19 20:02:10 +0100

    Create the ship with a class

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
+/* global Phaser */
 let PlayState = {};
 
+/*************************************************
+ *Ship sprite
+ ************************************************/
+function Ship(game, x, y) {
+  // call Phaser.Sprite parent constructor
+  Phaser.Sprite.call(this, game, x, y, 'ship');
+
+  this.anchor.setTo(0.5); // handle the sprite by its center
+  this.game.physics.arcade.enable(this); // enable physics
+  this.body.velocity.x = 100;
+}
+// inherit from Phaser.Sprite
+Ship.prototype = Object.create(Phaser.Sprite.prototype);
+Ship.prototype.constructor = Ship;
+
+/*************************************************
+ *Play game state
+ ************************************************/
 PlayState.preload = function() {
+  // load our image assets
   this.game.load.image('background', 'images/background.png');
   this.game.load.image('ship', 'images/ship.png');
 };
@@ -9,16 +29,13 @@ PlayState.create = function() {
   // create an image using the game's factory (.add)
   this.game.add.image(0, 0, 'background');
 
-  // create the sprite ship in the middle
-  this.ship = this.game.add.sprite(256, 436, 'ship');
-  this.ship.anchor.setTo(0.5); // handle the sprite by its center
-
-  // setup physics for the ship sprite
-  this.game.physics.arcade.enable(this.ship);
-  this.ship.body.velocity.x = 100;
+  // create the sprite ship
+  this.ship = new Ship(this.game, 256, 436);
+  // since we are instantiating the sprite and not using the factory method,
+  // we need to manually add it to the game world
+  this.game.add.existing(this.ship);
 };
 
 window.onload = function() {
-  // eslint-disable-next-line no-undef
   new Phaser.Game(512, 512, Phaser.AUTO, 'game', PlayState);
 };
